96d909e12f06a19905dc32185817f1f7f290c208
nandinierra <nandinierra34@gmail.com>
2025-12-23 12:01:13 +0530

    chore: Add new Vercel frontend URL to CORS allowed origins.

--- server.js
+++ server.js
@@ -3,38 +3,39 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import cors from "cors"; 
+import cors from "cors";
 import route from "./route/route.js"
-import {connectDb} from "./config/db.js";
+import { connectDb } from "./config/db.js";
 import authRoute from "./route/auth.route.js";
 import cookieParser from "cookie-parser"
 import cartroute from "./route/cart.route.js";
 import orderRoute from "./route/order.route.js";
 import profileRoute from "./route/profile.route.js";
-const app=express(); 
+const app = express();
 
-const port=process.env.PORT || 3060;
-const url=process.env.URL;
+const port = process.env.PORT || 3060;
+const url = process.env.URL;
 
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "https://quick-bite-frontend-ten.vercel.app"], 
-  credentials: true,}  ));
+  origin: ["http://localhost:5173", "http://localhost:5174", "https://quick-bite-frontend-ten.vercel.app", "https://quick-bite-frontend-eight.vercel.app"],
+  credentials: true,
+}));
 app.use(express.json())
-app.use(cookieParser()) 
+app.use(cookieParser())
 
 
-app.get("/", (req, res)=>{
-     res.status(201).json({
-        message:"got the get request"
-     })
+app.get("/", (req, res) => {
+  res.status(201).json({
+    message: "got the get request"
+  })
 })
 
-app.post("/post", (req,res)=>{
-    console.log(req.body)
-    res.status(201).json({
-        message:req.body
-    })
+app.post("/post", (req, res) => {
+  console.log(req.body)
+  res.status(201).json({
+    message: req.body
+  })
 })
 
 app.use("/foodItems", route);
@@ -45,7 +46,7 @@ app.use("/profile", profileRoute);
 
 connectDb(url);
 
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`server is runnig on port ${port}`)
-}) 
+})
 
